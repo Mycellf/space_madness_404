1d5688f7b8ad32c63f344ca0fb08bb52c8fa525c
Mycellf <jcouch210@gmail.com>
2024-03-01 15:31:14 -0900

    Added Camera

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,21 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
+    let texture = load_texture("assets/ship.png").await.unwrap();
+
+    let mut camera = Camera2D {
+        zoom: Vec2::splat(1.0 / 64.0),
+        ..Default::default()
+    };
+
     loop {
         clear_background(BLACK);
+
+        camera.zoom.x = camera.zoom.y / screen_width() * screen_height();
+
+        set_camera(&camera);
+
+        draw_texture(&texture, 0.0, 0.0, WHITE);
 
         next_frame().await;
     }
