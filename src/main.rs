59196dd964e66c30ffb06b176ccc25f7163738d7
Mycellf <jcouch210@gmail.com>
2024-03-01 15:46:59 -0900

    Added Material Code

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,30 @@ fn window_conf() -> Conf {
 async fn main() {
     let texture = load_texture("assets/ship.png").await.unwrap();
 
+    let fragment_shader = include_str!("shaders/fragment.glsl");
+    let vertex_shader = include_str!("shaders/vertex.glsl");
+
+    let pipeline_params = PipelineParams {
+        color_blend: Some(miniquad::BlendState::new(
+            miniquad::Equation::Add,
+            miniquad::BlendFactor::Value(miniquad::BlendValue::SourceAlpha),
+            miniquad::BlendFactor::OneMinusValue(miniquad::BlendValue::SourceAlpha),
+        )),
+        ..Default::default()
+    };
+
+    let material = load_material(
+        ShaderSource::Glsl {
+            vertex: &vertex_shader,
+            fragment: &fragment_shader,
+        },
+        MaterialParams {
+            pipeline_params,
+            ..Default::default()
+        },
+    )
+    .unwrap();
+
     let mut camera = Camera2D {
         zoom: Vec2::splat(1.0 / 64.0),
         ..Default::default()
@@ -22,6 +46,8 @@ async fn main() {
 
         camera.zoom.x = camera.zoom.y / screen_width() * screen_height();
         set_camera(&camera);
+
+        gl_use_material(&material);
 
         draw_texture(&texture, 0.0, 0.0, WHITE);
 
