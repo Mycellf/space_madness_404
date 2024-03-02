f6973f5e0692f91969a8a741f76f5b2882a2f5e0
Mycellf <jcouch210@gmail.com>
2024-03-02 10:30:57 -0900

    Added Physics Engine

---
+++ src/physics_world.rs
@@ -0,0 +1,57 @@
+use rapier2d::{na::Vector2, prelude::*};
+
+pub struct PhysicsWorld {
+    pub physics_pipeline: PhysicsPipeline,
+    pub integration_parameters: IntegrationParameters,
+    pub island_manager: IslandManager,
+    pub broad_phase: BroadPhase,
+    pub narrow_phase: NarrowPhase,
+    pub rigid_body_set: RigidBodySet,
+    pub collider_set: ColliderSet,
+    pub impulse_joint_set: ImpulseJointSet,
+    pub multibody_joint_set: MultibodyJointSet,
+    pub ccd_solver: CCDSolver,
+    pub query_pipeline: Option<QueryPipeline>,
+}
+
+impl PhysicsWorld {
+    pub fn new() -> Self {
+        Self {
+            physics_pipeline: PhysicsPipeline::new(),
+            integration_parameters: IntegrationParameters::default(),
+            island_manager: IslandManager::new(),
+            broad_phase: BroadPhase::new(),
+            narrow_phase: NarrowPhase::new(),
+            rigid_body_set: RigidBodySet::new(),
+            collider_set: ColliderSet::new(),
+            impulse_joint_set: ImpulseJointSet::new(),
+            multibody_joint_set: MultibodyJointSet::new(),
+            ccd_solver: CCDSolver::new(),
+            query_pipeline: Some(QueryPipeline::new()),
+        }
+    }
+
+    pub fn step(&mut self) {
+        self.physics_pipeline.step(
+            &Vector2::new(0.0, 0.0),
+            &self.integration_parameters,
+            &mut self.island_manager,
+            &mut self.broad_phase,
+            &mut self.narrow_phase,
+            &mut self.rigid_body_set,
+            &mut self.collider_set,
+            &mut self.impulse_joint_set,
+            &mut self.multibody_joint_set,
+            &mut self.ccd_solver,
+            self.query_pipeline.as_mut(),
+            &(),
+            &(),
+        )
+    }
+
+    pub fn add_rigidbody(&mut self, rigid_body: RigidBody, collider: Collider) {
+        let rigid_body_handle = self.rigid_body_set.insert(rigid_body);
+        self.collider_set
+            .insert_with_parent(collider, rigid_body_handle, &mut self.rigid_body_set);
+    }
+}
